dda36dc1b4f1b21c39a1f627b724f52fde5ce587
Emily Nguyen <hongmienft98@gmail.com>
2019-09-27 09:29:15 +0700

    Add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,15 @@ import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
