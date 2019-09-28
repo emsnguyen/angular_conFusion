aee267d8f7f4c955864d43a0fbc88817c6c549a9
Emily Nguyen <hongmienft98@gmail.com>
2019-09-28 08:09:36 +0700

    Add dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,8 @@ import { ContactComponent } from './contact/contact.component';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,11 +46,15 @@ import { ContactComponent } from './contact/contact.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService,
+  ],
+  entryComponents: [
+    LoginComponent,
   ],
   bootstrap: [AppComponent]
 })
